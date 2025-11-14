e8ccd40281f72c5342e322c5572bca6985639fea
Hazwan <hazwanazrielynt@gmail.com>
2025-11-15 00:56:38 +0700

    update

--- admin/dashboard_admin.php
+++ admin/dashboard_admin.php
@@ -18,7 +18,6 @@ $data_lubuk_baja = mysqli_fetch_assoc($query_lubuk_baja);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard Admin</title>
   <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
 <body>
   <div class="layout">  
